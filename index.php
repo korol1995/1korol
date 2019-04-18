e209b4df4d48091475ef8e5b9622d560c522c7b1
korol1995 <49747284+korol1995@users.noreply.github.com>
2019-04-18 11:10:31 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE>
+<html lang="ua">
+<?php include_once('head.php') ?>
+<footer>
+    <?php include_once('footer.php') ?>
+</footer>
+
+
+
+<div class="container content">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="/img/1.jpg" class="img-fluid" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="/img/2.jpg" class="img-fluid" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="/img/3.jpg" class="img-fluid" alt="...">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+
+
+
+
+
+
+</body>
+
+</html>
+
+
+
+
+
